449912f8de9012bb8937da3ac7af3bdeb20f6937
raulmejia <r8a8m8p8@gmail.com>
2019-08-02 17:11:46 +0200

    Df splited by ABCB1 expression levels and generic function

--- Orchester.R
+++ Orchester.R
@@ -24,96 +24,82 @@ if (!require("plotly")) {
 }
 
 
-args = commandArgs(trailingOnly=TRUE)
-df_path <-args[1]
+#args = commandArgs(trailingOnly=TRUE)
+#df_path <-args[1]
 
 TCGAem_file <-c("/media/rmejia/ADATA/boba-bk-postsismo/rmejia/Documents/Doctorado/ProyectoDoctorado/Pipe_post_IncosistencyPatways/4_Enrichment/DGE/Data/expMatrix_TCGA_cBioPortal_no_males.txt")
 TCGAem <-read.table(file=TCGAem_file, sep="\t", quote = "", stringsAsFactors = FALSE)
 TCGAem[1:5,1:4]
+TCGAem <- log(TCGAem + 1)
 
 
 ABCem <-TCGAem[grep("ABC",rownames(TCGAem)),]
 rownames(ABCem)
-ABCcolors<-c(rep("purple",14),rep("blue",9),rep("cyan",14),rep("green",4),rep("yellow",1),rep("pink",3),rep("red",5))
 
 
 sum(str_detect( colnames(ABCem) , "\\.01"))
 colnames(ABCem)[566]
 sum(str_detect( colnames(ABCem) , "\\.11"))
 
-str(ABCem[14,1:112])
-as.numeric(ABCem[14,1:112])
-boxplot(log2(as.numeric(ABCem[14,1:112]))+1 )
-boxplot(log2(as.numeric(ABCem[14,113:920]))+1 )
-
-par(mfrow=c(1,2))
-boxplot(as.numeric(ABCem[14,1:112])[order(as.numeric(ABCem[14,1:112]))])
-boxplot(as.numeric(ABCem[14,113:920])[order(as.numeric(ABCem[14,113:920]))])
-
-min(as.numeric(ABCem[14,1:112]) )
 
 colnames(ABCem)
-
 meltedABC <- melt(t(ABCem))
 colnames(meltedABC)[c(1,2,3)]<-c("Sample","Gene","Expression_Value")
 
-?melt
 head(meltedABC,n=5)
 meltedABC[910:930,]
 dim(meltedABC)
 meltedABC$health <- as.character(str_detect(meltedABC$Sample, "\\.11"))
-str(meltedABC$health)
-str(meltedABC$Sample)
 meltedABC$id_and_condition <- paste0(meltedABC$Sample , meltedABC$health)
 meltedABC$gene_and_condition <- paste0(meltedABC$Gene , meltedABC$health)
+#meltedABClog2[,"Expression_Value"]<- log2(meltedABClog2[,"Expression_Value"] +1)
 
-
-meltedABClog2<-meltedABC
-str(meltedABClog2)
-meltedABClog2[,"Expression_Value"]<- log2(meltedABClog2[,"Expression_Value"] +1)
-
-p1 <- ggplot(meltedABClog2[1:(10*920),], aes(Expression_Value, fill = gene_and_condition)) + geom_density(alpha = 0.2)
+## looking for the treshold
+p1 <- ggplot(meltedABC[1:(10*920),], aes(Expression_Value, fill = gene_and_condition)) + geom_density(alpha = 0.2)
 p1
-p2 <- ggplot(meltedABClog2[((10*920)+1):(20*920),], aes(Expression_Value, col= rep(c("green","red"),10) )) + geom_density(alpha = 0.2)
-p2
-p2 <- ggplot(meltedABClog2[((10*920)+1):(20*920),], aes(Expression_Value, fill=gene_and_condition)) + geom_density(alpha = 0.2)
-sick_health_col<-rep(c(rep("green",112),rep("red",808)),10)
+p2 <- ggplot(meltedABC[((10*920)+1):(20*920),], aes(Expression_Value, fill=gene_and_condition)) + geom_density(alpha = 0.2)
 p2
 
-ggplot(meltedABClog2[((10*920)+1):(20*920),], aes(x = Gene, y = Expression_Value, fill = health)) +
+ggplot(meltedABC[((10*920)+1):(20*920),], aes(x = Gene, y = Expression_Value, fill = health)) +
   geom_boxplot(outlier.size = 0) +
   geom_point(pch = 21, position = position_jitterdodge())
 
-ggplot(meltedABClog2[((0*920)+1):(25*920),], aes(x = Gene, y = Expression_Value, fill = health)) +
+ggplot(meltedABC[((0*920)+1):(25*920),], aes(x = Gene, y = Expression_Value, fill = health)) +
   geom_boxplot(outlier.size = 0) +
   geom_point(pch = 21, position = position_jitterdodge())
+
+pdf("/media/rmejia/ADATA/boba-bk-postsismo/rmejia/Documents/Otros_Proyectos_academicos/ABCB1/Results/ABCB1_boxplot_heallthvsTumours.pdf", height = 7 , width = 7)
+ggplot(meltedABC[((14*920)+1):(15*920),], aes(x = Gene, y = Expression_Value, fill = health)) +
+  geom_boxplot(outlier.size = 0) +
+  geom_point(pch = 21, position = position_jitterdodge())
+dev.off()
 
 pdf("/media/rmejia/ADATA/boba-bk-postsismo/rmejia/Documents/Otros_Proyectos_academicos/ABCB1/Results/boxplots_ABCs.pdf", height = 7 , width = 14)
-ggplot(data = meltedABClog2[((0*920)+1):(20*920),], aes(Gene, Expression_Value)) +
+ggplot(data = meltedABC[((0*920)+1):(20*920),], aes(Gene, Expression_Value)) +
   geom_boxplot(aes(colour = health))
-ggplot(data = meltedABClog2[((21*920)+1):(40*920),], aes(Gene, Expression_Value)) +
+ggplot(data = meltedABC[((21*920)+1):(40*920),], aes(Gene, Expression_Value)) +
   geom_boxplot(aes(colour = health))
-ggplot(data = meltedABClog2[((41*920)+1):(50*920),], aes(Gene, Expression_Value)) +
+ggplot(data = meltedABC[((41*920)+1):(50*920),], aes(Gene, Expression_Value)) +
   geom_boxplot(aes(colour = health))
 dev.off()
 
 
-pB1 <- ggplot(meltedABClog2[((14*920)+1):(15*920),], aes(Expression_Value, fill = gene_and_condition)) + geom_density(alpha = 0.2)
+pB1 <- ggplot(meltedABC[((14*920)+1):(15*920),], aes(Expression_Value, fill = gene_and_condition)) + geom_density(alpha = 0.2)
 pB1
-pB1 <- ggplot(meltedABClog2[((14*920)+1):(15*920),], aes(y=Expression_Value, fill = gene_and_condition)) + geom_boxplot(alpha = 0.2)
+pB1 <- ggplot(meltedABC[((14*920)+1):(15*920),], aes(y=Expression_Value, fill = gene_and_condition)) + geom_boxplot(alpha = 0.2)
 pB1
-pB1 <- ggplot(meltedABClog2[((14*920)+1):(15*920),], aes(y=Expression_Value, x = gene_and_condition)) + geom_violin(alpha = 0.2)
+pB1 <- ggplot(meltedABC[((14*920)+1):(15*920),], aes(y=Expression_Value, x = gene_and_condition)) + geom_violin(alpha = 0.2)
 pB1
 pB1 + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.1, binwidth = 0.1)
 
-p3 <- ggplot(meltedABClog2[((20*920)+1):(30*920),], aes(Expression_Value, fill = Gene)) + geom_density(alpha = 0.2)
+p3 <- ggplot(meltedABC[((20*920)+1):(30*920),], aes(Expression_Value, fill = Gene)) + geom_density(alpha = 0.2)
 p3
-p4 <- ggplot(meltedABClog2[((30*920)+1):(40*920),], aes(Expression_Value, fill = Gene)) + geom_density(alpha = 0.2)
+p4 <- ggplot(meltedABC[((30*920)+1):(40*920),], aes(Expression_Value, fill = Gene)) + geom_density(alpha = 0.2)
 p4
-p4 <- ggplot(meltedABClog2[((32*920)+1):(34*920),], aes(Expression_Value, fill = Gene)) + geom_density(alpha = 0.2)
+p4 <- ggplot(meltedABC[((32*920)+1):(34*920),], aes(Expression_Value, fill = Gene)) + geom_density(alpha = 0.2)
 p4
 # Interesante ABCC6 y ABCC6P1 tienen distribuciones bimodales, que pacientes seran?
-p5 <- ggplot(meltedABClog2[((49*920)+1):(50*920),], aes(Expression_Value, fill = Gene)) + geom_density(alpha = 0.2)
+p5 <- ggplot(meltedABC[((49*920)+1):(50*920),], aes(Expression_Value, fill = Gene)) + geom_density(alpha = 0.2)
 p5
 # La distribucion de ABCG8 tambien esta curiosa (el 50)
 # p5 nada solo unas de piquitos finales
@@ -151,4 +137,17 @@ p4
 # Interesante ABCC6 y ABCC6P1 tienen distribuciones bimodales, que pacientes seran?
 p5 <- ggplot(meltedABClog2tum[((47*920)+1):(49*920),], aes(Expression_Value, fill = Gene)) + geom_density(alpha = 0.2)
 p5
+
+##### Extracting the more 
+
+source("/media/rmejia/ADATA/boba-bk-postsismo/rmejia/Documents/Otros_Proyectos_academicos/ABCB1/ABCB1_code/splitdf_by_gene_level_in_tumours.R")
+# let's split our data frame acoording to the levels of ABCB1, 
+# we will build subdata frames, that only keep tumours with a ABCB1 expression level above the mean , under the mean , and so on...
+list_TCGA_splited_by_ABCB1_levels_in_tumours <- splitdf_by_gene_level_in_tumours(TCGAem, 1:112, 113:920, "ABCB1")
+
+lapply(list_TCGA_splited_by_ABCB1_levels_in_tumours,dim) # let's check
+list_TCGA_splited_by_ABCB1_levels_in_tumours[[1]][1:5,1:5]
+
+## Now pathifier
+
 
---
+++ splitdf_by_gene_level_in_tumours.R
@@ -0,0 +1,33 @@
+
+Split_by_thisgene_level<- function( mymatrix, gene){
+  mymatrix_25th_top_high <- mymatrix[ , mymatrix[gene,] > quantile(as.matrix(mymatrix[gene,]) , .75) ]
+  mymatrix_high <- mymatrix[, mymatrix[gene,] > mean(as.matrix(mymatrix[gene,]))]
+  mymatrix_low <- mymatrix[,mymatrix[gene,] < mean(as.matrix( mymatrix[gene,]))]
+  mymatrix_25th_top_low <- mymatrix[ , mymatrix[gene,] < quantile(as.matrix(mymatrix[gene,]) , .25) ]
+  result<- list(mymatrix_25th_top_high, mymatrix_high, mymatrix_low, mymatrix_25th_top_low)
+  names(result) <- c("25th_top_high","high","low","25th_top_low")
+  return(result)
+}
+
+merge_with_healthem <- function(list_splited, healthem){
+  #list_splited <- list_of_splited_TCGAem_Tumour_by_ABCB1_levels
+  #healthem <- TCGAem_Health
+  list_splited[["25th_top_high"]] <- cbind(list_splited[["25th_top_high"]] , healthem )
+  list_splited[["high"]] <- cbind(list_splited[["high"]] , healthem )
+  list_splited[["25th_top_low"]] <- cbind(list_splited[["25th_top_low"]] , healthem )
+  list_splited[["low"]] <- cbind(list_splited[["low"]] , healthem )
+  return(list_splited)
+}
+
+splitdf_by_gene_level_in_tumours <- function(df, healthy_positions, tumour_positions,mygene){
+  #This function split a data frame in health and disease, and create sub data frames of the
+  # disease for example the 4th df has eliminated all the patients (columns) if 
+  # their level of the choosed gene is below than the mean, below than 25 th percentil
+  # then that submatrix will be pasted to the Healthy df
+  # the function returns a list whose elements are each data frame  according that condition
+  
+  df_Health <- df[,healthy_positions]
+  df_Tumour <- df[,tumour_positions]
+  list_of_splited_df_Tumour_by_thisgene_levels <- Split_by_thisgene_level(df_Tumour,mygene)
+  list_of_splited_df_by_thisgene_levels <- merge_with_healthem(list_of_splited_df_Tumour_by_thisgene_levels, TCGAem_Health)
+}
