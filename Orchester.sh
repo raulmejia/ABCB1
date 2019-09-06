ea9939363866650119d5f6feb357728e45612d3c
raulmejia <r8a8m8p8@gmail.com>
2019-09-06 20:06:25 +0200

    upgrading Orchester

--- Orchester.sh
+++ Orchester.sh
@@ -16,8 +16,7 @@ Rscript Pathifier_Args_10stabilizing_Filtervalue3_75.R ../Results/Matrices_split
 
 Rscript Pathifier_Args_10stabilizing_Filtervalue3_75.R ../Results/Matrices_splited_by_gene/ABCB1/TCGA_Basal_splited_by_the_expression_of_the_gene_ABCB1_high.tsv ../Results/KEGGDB/KEGG_pathways_in_df_genesymbol.tsv ./  ../Results/Pathifier/Basal/TCGA/ TCGA_Basal_above_percentile_50_stbl_10
 
-Rscript Pathifier_Args_10stabilizing_Filtervalue3_75.R ../Results/Matrices_splited_by_gene/ABCB1/TCGA_Basal_splited_by_the_expression_of_the_gene_ABCB1_25th_top_high.tsv 
- ../Results/KEGGDB/KEGG_pathways_in_df_genesymbol.tsv ./  ../Results/Pathifier/Basal/TCGA/ TCGA_Basal_above_percentile_75_stbl_10
+Rscript Pathifier_Args_10stabilizing_Filtervalue3_75.R ../Results/Matrices_splited_by_gene/ABCB1/TCGA_Basal_splited_by_the_expression_of_the_gene_ABCB1_25th_top_high.tsv ../Results/KEGGDB/KEGG_pathways_in_df_genesymbol.tsv ./ ../Results/Pathifier/Basal/TCGA/ TCGA_Basal_above_percentile_75_stbl_10
 
 
 for names in ../Results/Splited/SubMatrices_with_controls/Subexpression_matrix_*from_TCGA_.tsv ; do
