b57e06704697a5f92a38cb89df48a0c0100a0c52
raulmejia <r8a8m8p8@gmail.com>
2019-09-09 04:30:46 +0200

    mod too orchy

--- Orchester.sh
+++ Orchester.sh
@@ -76,13 +76,25 @@ Rscript Pathifier_Args_10stabilizing_Filtervalue3.R ../Results/Matrices_splited_
 ## only log 2 transformed
 #Usar limma en lugar de DESeq2
 
-Rscript DGE_limma.R  ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_25th_top_low.tsv ../Data/Labels_Ctrl_and_NL_Recal_separated_METABRIC.txt ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
+Rscript DGE_limma2.R ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_25th_top_low.tsv ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
 
-Rscript DGE_limma.R  ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_low.tsv ../Data/Labels_Ctrl_and_NL_Recal_separated_METABRIC.txt ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
+Rscript DGE_limma2.R ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_low.tsv ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
 
-Rscript DGE_limma.R  ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_high.tsv ../Data/Labels_Ctrl_and_NL_Recal_separated_METABRIC.txt ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
+Rscript DGE_limma2.R ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_high.tsv ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
 
-Rscript DGE_limma.R  ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_25th_top_high.tsv ../Data/Labels_Ctrl_and_NL_Recal_separated_METABRIC.txt ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
+Rscript DGE_limma2.R ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_25th_top_high.tsv ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
+
+
+
+
+
+Rscript DGE_limma.R ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_25th_top_low.tsv ../Data/Labels_Ctrl_and_NL_Recal_separated_METABRIC.txt ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
+
+Rscript DGE_limma.R ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_low.tsv ../Data/Labels_Ctrl_and_NL_Recal_separated_METABRIC.txt ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
+
+Rscript DGE_limma.R ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_high.tsv ../Data/Labels_Ctrl_and_NL_Recal_separated_METABRIC.txt ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
+
+Rscript DGE_limma.R ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_splited_by_the_expression_of_the_gene_ABCB1_25th_top_high.tsv ../Data/Labels_Ctrl_and_NL_Recal_separated_METABRIC.txt ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_Basal_ABCB1_under_per25_lfc0_6_padj0_05
 
 
 ## Big DF Pathway Target Drug Interaction LogFC 
@@ -90,6 +102,7 @@ Rscript DGE_limma.R  ../Results/Matrices_splited_by_gene/ABCB1/METABRIC_Basal_sp
 Rscript DfPathways__and_PwDefinitions_and_dfDEG_2_Df_of_PwGenesDrugs.R ../Results/KEGGDB/KEGG_pathways_in_df_genesymbol.tsv ../Results/Pathifier/Basal/TCGA/TCGA_Basal_under_percentile_25_stbl_10_median_PDSz_ordered_matrix_Top20.txt ../Results/DEG/TCGA/log2only/padj10_3_lfc1_results_DESeq_DGE_TCGA_Basal_ABCB1_under_per25_only_log2transformed_lfc2_of0_6_padjof0_05.tsv ./ ../Results/BigDfPTD/METABRIC/ METABRIC_Basal_under_percentile_25_top20_DEG_log2only
 
 
+Rscript DGE_limma2.R ../Data/toyMETABRIC_Controls_LumA.txt ./ ../Results/DEG/METABRIC/ 0.6 0.05 METABRIC_toy_Normals_LumisA
 
 
 
