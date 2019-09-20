99232447879d41f0758f3e8d830c7d9bb19f4a56
raulmejia <r8a8m8p8@gmail.com>
2019-09-20 09:55:51 -0500

    seminario

--- Pheatmap.R
+++ Pheatmap.R
@@ -7,14 +7,13 @@
 ###########################################
 args <- commandArgs(trailingOnly = TRUE)
 Path_to_your_Matrix<-args[1] # The path to your matrix
-# Path_to_your_Matrix<-c("../Results/Splited/Submatrices_ohne_controls/Subexpression_matrix_Basal_from_METABRIC_.tsv_only_ABC_transporters_genes.tsv") ; Path_of_Code<-c("./") ; Path_of_Results<-c("../Results/Clusterig/") ; Labels <- "METABRIC_Only_Basal_Only_ABC_transporters"
-# Path_to_your_Matrix<-c("Control_with_subexpression_matrix_of_Basal_from_METABRIC_.tsv_only_ABC_transporters_genes.tsv")
-# Path_to_your_Matrix <- choose.files() # choose.dir()
-Path_of_Code<-args[2] # The path to your code
-Path_of_Results<-args[3] # # where do you want to save your results?
-# Path_of_Results <- choose.dir()
-# choose.dir(default = "", caption = "Select folder")
+Path_to_myPhenoData <- args[2] # The path to your Feature data
+Path_of_Code<-args[3] # The path to your code
+Path_of_Results<-args[4] # # where do you want to save your results?
 Labels <-args[4] # Label for your results
+# Path_to_your_Matrix<-c("../Results/Splited/Submatrices_ohne_controls/Subexpression_matrix_Basal_from_METABRIC_.tsv_only_ABC_transporters_genes.tsv") ; Path_to_myPhenoData <- c("../Results/Lehmann-STROMA4/METABRIC/Lehmann\'s_Subt_and_properties_Numerical_METABRIC-only-Basals-lehmann_s_and_properties.tsv") ; Path_of_Code<-c("./") ; Path_of_Results<-c("../Results/Clusterig/METABRIC/Heatmaps/Basal_Only_ABCS/") ; Labels <- "Heatmap_METABRIC_Basal_ABC_transporters_Lehmann"
+# Path_to_your_Matrix <- choose.files() # choose.dir()
+
 
 ###############################################################################
 ### Installing and/or loading required packages
@@ -43,22 +42,47 @@ if (!require("pheatmap")) {
   BiocManager::install("pheatmap", dependencies = TRUE)
   library(pheatmap)
 }
+if (!require("dplyr")) {
+  BiocManager::install("dplyr", dependencies = TRUE)
+  library(dplyr)
+}
 
 ########################
 ### Reading the data and creating the folder for the results
 ########################
-library("Hiiragi2013")
-data("x")
-x
-str(x)
-dim(Biobase::exprs(x))
-class(Biobase::exprs(x))
-Biobase::exprs(x)[1:10,1:10]
-attributes(x)
+dir.create(Path_of_Results , recursive = TRUE )
+M.matrix<-read.table(Path_to_your_Matrix,header=TRUE,row.names = 1)
 
-library("pheatmap")
+## Feature 
+myPhenoData <-read.table(Path_to_myPhenoData ,header=TRUE,row.names = 1)
+myPhenoData <- as.data.frame(myPhenoData )
+My_pData_Anotdf <- AnnotatedDataFrame( data=myFeatureData)
+validObject(My_fData_Anotdf)
+##################################
+### Building ExpressionSet Object
+##################################
+myExpressionSet <- ExpressionSet(as.matrix( M.matrix ), phenoData= My_pData_Anotdf  )
+
+########################
+### Pheatmap
+########################
+
 topGenes = order(rowVars(Biobase::exprs(x)), decreasing = TRUE)[1:500]
 rowCenter = function(x) { x - rowMeans(x) }
+?pheatmap
+max(rowCenter(Biobase::exprs(x)[ topGenes, ]))
+min(rowCenter(Biobase::exprs(x)[ topGenes, ]))
+dim( rowCenter(Biobase::exprs(x)[ topGenes, ]))
+mtcars %>%
+  group_by(cyl) %>%
+  summarise(mean = mean(disp), n = n())
+?summarise
+groups = group_by(pData(x), sampleGroup) %>%
+  summarise(n = n(), color = unique(sampleColour))
+groups
+groupColor = setNames(groups$color, groups$sampleGroup)
+vignette("programming")
+?pheatmap
 pheatmap( rowCenter(Biobase::exprs(x)[ topGenes, ] ),
           show_rownames = FALSE, show_colnames = FALSE,
           breaks = seq(-5, +5, length = 101),
@@ -74,8 +98,6 @@ pheatmap( rowCenter(Biobase::exprs(x)[ topGenes, ] ),
           ),
           cutree_rows = 4
 )
-
-
 
 dir.create(Path_of_Results , recursive = TRUE )
 M.matrix<-read.table(Path_to_your_Matrix,header=TRUE,row.names = 1)
