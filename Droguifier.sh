a5e0b1c48bba2a9452f87afded578084d90a9a61
raulmejia <r8a8m8p8@gmail.com>
2019-10-04 20:58:08 -0500

    droggifier outline of comments

---
+++ Droguifier.sh
@@ -0,0 +1,11 @@
+# A) ESet pieces to Expression matrix of cases and controls
+
+# B) Pathifier
+
+# C) Differential expressed genes
+
+# D) Drugs annotation
+
+# E) homeo-antihomeo
+
+# F) Clustering and ploting a set of genes ¿And using the BigDf for annotation?
